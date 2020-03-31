1f32560eb703a65596d48ffef26fd7a4055ab368
Shaykhnazar <49790448+Shaykhnazar@users.noreply.github.com>
2020-03-31 21:43:56 +0500

    migrations (ticket, comment) without foreign key

---
+++ database/migrations/2020_03_30_120631_create_tickets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('subject');
+            $table->text('message');
+            $table->string('file');
+            $table->string('status');
+            $table->string('slug')->unique();
+            $table->integer('comment_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->integer('manager_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
