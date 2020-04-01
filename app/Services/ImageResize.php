8c51115d54a4e6177dd3adb000732ff2500a0251
Shaykhnazar <49790448+Shaykhnazar@users.noreply.github.com>
2020-04-01 16:16:07 +0500

    Added ImageResize Service

--- app/Services/ImageResize.php
+++ app/Services/ImageResize.php
@@ -4,32 +4,63 @@ namespace App\Services;
 
 use Image;
 
+/**
+ * ImageResize class
+ */
 class ImageResize
 {
 
+    /**
+     * Resize image as Square(fit) or Rectangle(resize)
+     * with this functions
+     *
+     * @param  mixed $img_name
+     * @param  mixed $x
+     * @param  mixed $y
+     * @param  mixed $type
+     * @return void
+     */
     public static function crop($img_name, $x, $y, $type = 'fit')
     {
         $full_path = storage_path('app/public/'.$img_name);
         $full_thumb_path = storage_path('app/public/thumbs/'.$img_name);
         $thumb = Image::make($full_path);
-        //Kvadrat
+
         if ($type == 'fit')
             self::fit($thumb, $x, $y, $full_thumb_path);
         else
             self::resize($thumb, $x, $y, $full_thumb_path);
     }
 
+    /**
+     * Resize image as Square by params $x, $y
+     *
+     * @param  mixed $thumb
+     * @param  mixed $x
+     * @param  mixed $y
+     * @param  mixed $full_thumb_path
+     * @return void
+     */
     private static function fit($thumb, $x, $y, $full_thumb_path)
     {
-        //Kvadrat proporsiya
+
         $thumb->fit($x, $y, function($constraint){
             $constraint->aspectRatio();
         })->save($full_thumb_path);
     }
 
+    /**
+     * Resize image as Rectangle by params $x, $y
+     *
+     * @param  mixed $thumb
+     * @param  mixed $x
+     * @param  mixed $y
+     * @param  mixed $full_thumb_path
+     * @return void
+     */
     private static function resize($thumb, $x, $y, $full_thumb_path)
     {
-        //Proporsiya
+
         $thumb->resize($x, $y, function($constraint) {
             $constraint->aspectRatio();
         })->save($full_thumb_path);
